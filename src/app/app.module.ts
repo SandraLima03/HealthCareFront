f849e955b67fca76a38459c34a60ed1d8e5bcb82
Maria <malu.olicosta@gmail.com>
2022-12-21 13:48:49 -0300

    deploy cadastro hospital

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CadMedicoComponent } from './screens/cad-medico/cad-medico.component';
 import { TextComponent } from './component/input/text/text.component';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import { HeaderScreenComponent } from './component/header-screen/header-screen.component';
+import { CadHospitalComponent } from './screens/cad-hospital/cad-hospital.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,6 +25,7 @@ import { HeaderScreenComponent } from './component/header-screen/header-screen.c
     CadMedicoComponent,
     TextComponent,
     HeaderScreenComponent,
+    CadHospitalComponent
   ],
   imports: [
     CommonModule,
--- src/utils/sidebar-links.ts
+++ src/utils/sidebar-links.ts
@@ -15,7 +15,7 @@ export const sidebarLinks: SidebarLink[] = [
   {name: 'Cadastro beneficiário', url: 'cadastro-beneficiario', icon: 'account_circle'},
   {name: 'Agendamento', url: '/', icon: 'event'},
   {name: 'Painel Administrativo', url: '/', icon: 'supervised_user_circle', iconDrop: 'keyboard_arrow_down', submenu: [
-    {name: 'Cadastrar Hospital', url: '/'},
+    {name: 'Cadastrar Hospital', url: 'cadastro-hospital'},
     {name: 'Cadastrar Médico', url: '/'},
     {name: 'Cadastrar Especialidade', url: '/'}
   ]}
