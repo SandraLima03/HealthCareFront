ff8afdd1b46fd7082d8d8151cbab2325a4dd0c43
Maria <malu.olicosta@gmail.com>
2022-12-21 15:33:52 -0300

    deploy cadastro especialidade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
-import { CadHospitalComponent } from './screens/cad-hospital/cad-hospital.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {CadBeneficiarioComponent} from './screens/cad-beneficiario/cad-beneficiario.component';
 import {HomeComponent} from './screens/home/home.component'
+import { CadHospitalComponent } from './screens/cad-hospital/cad-hospital.component';
+import { CadEspecialidadeComponent } from './screens/cad-especialidade/cad-especialidade.component';
 
 const routes: Routes = [
   {path: 'cadastro-beneficiario', component:CadBeneficiarioComponent},
   {path: '', component:HomeComponent},
   {path: 'cadastro-hospital', component:CadHospitalComponent},
+  {path: '', component:HomeComponent},
+  {path: 'cadastro-especialidade', component:CadEspecialidadeComponent},
   {path: '', component:HomeComponent}
 ];
 
